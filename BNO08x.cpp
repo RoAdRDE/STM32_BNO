09ce9c70215d1610f82c1069684476f21ec018b5
RoAdRDE <rudraksh21193@iiitd.ac.in>
2023-06-16 20:33:35 +0530

    //TINTIN PLS ADD FUNCTION

---
+++ BNO08x.cpp
@@ -0,0 +1,86 @@
+#include "BNO08x.h"
+#include <cmath>
+
+bool BNO085::beginSPI(uint8_t user_CSPin, uint8_t user_WAKPin, uint8_t user_INTPin, uint8_t user_RSTPin)
+{
+    //TINTIN
+}
+
+float BNO085::qToFloat(int16_t fixedPointValue, uint8_t qPoint)
+{
+	float qFloat = fixedPointValue;
+	qFloat *= pow(2, qPoint * -1);
+	return (qFloat);
+}
+
+bool BNO085::receivePacket(void)
+{
+     //TINTIN
+}
+
+bool BNO085::enableRotationVector(uint16_t timeBetweenReports)
+{
+	bool status = setFeatureCommand(SENSOR_REPORTID_ROTATION_VECTOR, timeBetweenReports);
+	return status;
+}
+
+bool BNO085::enableAccelerometer(uint16_t timeBetweenReports)
+{
+	bool status = setFeatureCommand(SENSOR_REPORTID_ACCELEROMETER, timeBetweenReports);
+	return status;
+}
+
+bool BNO085::enableLinearAccelerometer(uint16_t timeBetweenReports)
+{
+	bool status = setFeatureCommand(SENSOR_REPORTID_LINEAR_ACCELERATION, timeBetweenReports);
+	return status;
+}
+
+bool BNO085::enableGravity(uint16_t timeBetweenReports)
+{
+	bool status = setFeatureCommand(SENSOR_REPORTID_GRAVITY, timeBetweenReports);
+	return status;
+}
+
+bool BNO085::enableGyro(uint16_t timeBetweenReports)
+{
+	bool status = setFeatureCommand(SENSOR_REPORTID_GYROSCOPE, timeBetweenReports);
+	return status;
+}
+
+bool BNO085::setFeatureCommand(uint8_t reportID, uint16_t timeBetweenReports)
+{
+	bool status = setFeatureCommand(reportID, timeBetweenReports, 0); //No specific config
+	return status;
+}
+
+bool BNO085::setFeatureCommand(uint8_t reportID, uint16_t timeBetweenReports, uint32_t specificConfig)
+{
+	long microsBetweenReports = (long)timeBetweenReports * 1000L;
+
+	shtpData[0] = SHTP_REPORT_SET_FEATURE_COMMAND;	 //Set feature command. Reference page 55
+	shtpData[1] = reportID;							   //Feature Report ID. 0x01 = Accelerometer, 0x05 = Rotation vector
+	shtpData[2] = 0;								   //Feature flags
+	shtpData[3] = 0;								   //Change sensitivity (LSB)
+	shtpData[4] = 0;								   //Change sensitivity (MSB)
+	shtpData[5] = (microsBetweenReports >> 0) & 0xFF;  //Report interval (LSB) in microseconds. 0x7A120 = 500ms
+	shtpData[6] = (microsBetweenReports >> 8) & 0xFF;  //Report interval
+	shtpData[7] = (microsBetweenReports >> 16) & 0xFF; //Report interval
+	shtpData[8] = (microsBetweenReports >> 24) & 0xFF; //Report interval (MSB)
+	shtpData[9] = 0;								   //Batch Interval (LSB)
+	shtpData[10] = 0;								   //Batch Interval
+	shtpData[11] = 0;								   //Batch Interval
+	shtpData[12] = 0;								   //Batch Interval (MSB)
+	shtpData[13] = (specificConfig >> 0) & 0xFF;	   //Sensor-specific config (LSB)
+	shtpData[14] = (specificConfig >> 8) & 0xFF;	   //Sensor-specific config
+	shtpData[15] = (specificConfig >> 16) & 0xFF;	  //Sensor-specific config
+	shtpData[16] = (specificConfig >> 24) & 0xFF;	  //Sensor-specific config (MSB)
+
+	//Transmit packet on channel 2, 17 bytes
+	bool status = sendPacket(CHANNEL_CONTROL, 17);
+	return status;
+}
+
+
+
+
---
+++ BNO08x.h
@@ -0,0 +1,93 @@
+#include <stdint.h>
+
+const uint8_t CHANNEL_COMMAND = 0;
+const uint8_t CHANNEL_EXECUTABLE = 1;
+const uint8_t CHANNEL_CONTROL = 2;
+const uint8_t CHANNEL_REPORTS = 3;
+const uint8_t CHANNEL_WAKE_REPORTS = 4;
+const uint8_t CHANNEL_GYRO = 5;
+
+#define SHTP_REPORT_COMMAND_RESPONSE 0xF1
+#define SHTP_REPORT_COMMAND_REQUEST 0xF2
+#define SHTP_REPORT_FRS_READ_RESPONSE 0xF3
+#define SHTP_REPORT_FRS_READ_REQUEST 0xF4
+#define SHTP_REPORT_PRODUCT_ID_RESPONSE 0xF8
+#define SHTP_REPORT_PRODUCT_ID_REQUEST 0xF9
+#define SHTP_REPORT_BASE_TIMESTAMP 0xFB
+#define SHTP_REPORT_SET_FEATURE_COMMAND 0xFD
+
+#define SENSOR_REPORTID_ACCELEROMETER 0x01
+#define SENSOR_REPORTID_GYROSCOPE 0x02
+#define SENSOR_REPORTID_MAGNETIC_FIELD 0x03
+#define SENSOR_REPORTID_LINEAR_ACCELERATION 0x04
+#define SENSOR_REPORTID_ROTATION_VECTOR 0x05
+#define SENSOR_REPORTID_GRAVITY 0x06
+#define SENSOR_REPORTID_GAME_ROTATION_VECTOR 0x08
+#define SENSOR_REPORTID_GEOMAGNETIC_ROTATION_VECTOR 0x09
+#define SENSOR_REPORTID_GYRO_INTEGRATED_ROTATION_VECTOR 0x2A
+#define SENSOR_REPORTID_TAP_DETECTOR 0x10
+#define SENSOR_REPORTID_STEP_COUNTER 0x11
+#define SENSOR_REPORTID_STABILITY_CLASSIFIER 0x13
+#define SENSOR_REPORTID_RAW_ACCELEROMETER 0x14
+#define SENSOR_REPORTID_RAW_GYROSCOPE 0x15
+#define SENSOR_REPORTID_RAW_MAGNETOMETER 0x16
+#define SENSOR_REPORTID_PERSONAL_ACTIVITY_CLASSIFIER 0x1E
+#define SENSOR_REPORTID_AR_VR_STABILIZED_ROTATION_VECTOR 0x28
+#define SENSOR_REPORTID_AR_VR_STABILIZED_GAME_ROTATION_VECTOR 0x29
+
+#define FRS_RECORDID_ACCELEROMETER 0xE302
+#define FRS_RECORDID_GYROSCOPE_CALIBRATED 0xE306
+#define FRS_RECORDID_MAGNETIC_FIELD_CALIBRATED 0xE309
+#define FRS_RECORDID_ROTATION_VECTOR 0xE30B
+
+#define EXECUTABLE_RESET_COMPLETE 0x1
+
+#define COMMAND_ERRORS 1
+#define COMMAND_COUNTER 2
+#define COMMAND_TARE 3
+#define COMMAND_INITIALIZE 4
+#define COMMAND_DCD 6
+#define COMMAND_ME_CALIBRATE 7
+#define COMMAND_DCD_PERIOD_SAVE 9
+#define COMMAND_OSCILLATOR 10
+#define COMMAND_CLEAR_DCD 11
+
+#define CALIBRATE_ACCEL 0
+#define CALIBRATE_GYRO 1
+#define CALIBRATE_MAG 2
+#define CALIBRATE_PLANAR_ACCEL 3
+#define CALIBRATE_ACCEL_GYRO_MAG 4
+#define CALIBRATE_STOP 5
+
+#define MAX_PACKET_SIZE 128 
+#define MAX_METADATA_SIZE 9
+
+uint8_t shtpHeader[4]; //Each packet has a header of 4 bytes
+uint8_t shtpData[MAX_PACKET_SIZE];
+uint8_t sequenceNumber[6] = {0, 0, 0, 0, 0, 0}; //There are 6 com channels. Each channel has its own seqnum
+uint8_t commandSequenceNumber = 0;				//Commands have a seqNum as well. These are inside command packet, the header uses its own seqNum per channel
+uint32_t metaData[MAX_METADATA_SIZE];
+
+class BNO085
+{
+    public:
+        	bool beginSPI(uint8_t user_CSPin, uint8_t user_WAKPin, uint8_t user_INTPin, uint8_t user_RSTPin); //TINTIN
+             // Reset Functions to be added 
+            float qToFloat(int16_t fixedPointValue, uint8_t qPoint);
+            // Wait for SPI
+            bool receivePacket(void); //TINTIN
+            bool sendPacket(uint8_t channelNumber, uint8_t dataLength); //TINTIN
+
+            bool setFeatureCommand(uint8_t reportID, uint16_t timeBetweenReports);
+	        bool setFeatureCommand(uint8_t reportID, uint16_t timeBetweenReports, uint32_t specificConfig);
+
+            bool enableRotationVector(uint16_t timeBetweenReports);
+            bool enableAccelerometer(uint16_t timeBetweenReports);
+            bool enableLinearAccelerometer(uint16_t timeBetweenReports);
+            bool enableGravity(uint16_t timeBetweenReports);
+            bool enableGyro(uint16_t timeBetweenReports);
+            bool enableMagnetometer(uint16_t timeBetweenReports);
+
+
+
+};
